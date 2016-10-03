d2958e797b6c18e163e0963181e5b1b5bf062781
Jared Grippe <jared@deadlyicon.com>
2016-10-03 15:04:14 -0700

    WIP

--- browser/components/Link.js
+++ browser/components/Link.js
@@ -1,4 +1,27 @@
-import { Link } from 'react-router'
+import React, { Component } from 'react'
 
-// we'll wrap this eventually
+class Link extends Component {
+
+  static PropTypes = {
+    to:      React.PropTypes.string,
+    href:    React.PropTypes.string,
+    path:    React.PropTypes.string,
+    onClick: React.PropTypes.func,
+  }
+
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  render(){
+    const props = Object.assign({}, this.props)
+    props.href = props.href || props.to || ''
+    props.onClick = this.onClick
+    return <a {...props}>{props.children}</a>
+  }
+
+}
+
+
 export default Link
